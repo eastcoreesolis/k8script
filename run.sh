d7cb3f8ff138fb4597d13eccedd89459220ecf28
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2019-12-27 14:45:15 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-echo "sleeping..." > sleeping.txt
-sleep 1m
-echo "end sleep." > end_sleep.txt
+echo "-Log Start-..." > log.txt
+echo "apt-get update..." >> log.txt
 sudo apt-get update -y
+echo "Getting K8s..." >> log.txt
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
+echo "Getting docker..." >> log.txt
 sudo apt-get update && sudo apt-get install docker.io -y
+echo "Getting minikube..." >> log.txt
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
-minikube version > ver.txt
+minikube version >> log.txt
+echo "Starting minikube..." >> log.txt
 sudo minikube start --vm-driver=none
-minikube status > ver2.txt
-sleep 1m
+minikube status >> log.txt
 git clone "https://github.com/eastcoreesolis/k8script.git"
-kubectl create -f ./k8script/namespace.yml
-kubectl --namespace=timeserv create -f ./k8script/service.yml
-kubectl --namespace=timeserv create -f ./k8script/deployment.yml
-sleep 1m
-kubectl --namespace=timeserv get pods > podlog.txt
+kubectl create -f ./k8script/namespace.yml >> log.txt
+kubectl --namespace=timeserv create -f ./k8script/service.yml >> log.txt
+kubectl --namespace=timeserv create -f ./k8script/deployment.yml >>log.txt
+echo "sleeping 30s..." >> log.txt
+sleep 30s
+echo "Check status..." >> log.txt
+kubectl --namespace=timeserv get pods >> log.txt
+echo "-Log End-..." > log.txt
