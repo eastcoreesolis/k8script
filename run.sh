ecd87fc2d0fa77af5f8eb59e7029b2cb74c08c8a
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2019-12-27 14:29:33 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "sleeping..." > sleeping.txt
-sleep 2m
+sleep 1m
 echo "end sleep." > end_sleep.txt
 sudo apt-get update -y
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
@@ -11,7 +11,7 @@ curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/miniku
 minikube version > ver.txt
 sudo minikube start --vm-driver=none
 minikube status > ver2.txt
-sleep 2m
+sleep 1m
 git clone "https://github.com/eastcoreesolis/k8script.git"
 kubectl create -f ./k8script/namespace.yml
 kubectl --namespace=timeserv create -f ./k8script/service.yml
