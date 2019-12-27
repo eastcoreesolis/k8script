6c3dd314519187452c92b528c43d2f6dff3329e6
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2019-12-27 16:20:37 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -23,14 +23,15 @@ minikube status >> log.txt
 echo "//" >> log.txt
 echo "Getting App..." >> log.txt
 git clone "https://github.com/eastcoreesolis/k8script.git"
+echo "//" >> log.txt
 echo "Creating App..." >> log.txt
 kubectl create -f ./k8script/namespace.yml >> log.txt
 kubectl --namespace=timeserv create -f ./k8script/service.yml >> log.txt
 kubectl --namespace=timeserv create -f ./k8script/deployment.yml >>log.txt
 echo "//" >> log.txt
-echo "sleeping 45s..." >> log.txt
+echo "Sleeping 45s..." >> log.txt
 sleep 45s
-echo "sleep complete, checking status..." >> log.txt
+echo "Checking pod status..." >> log.txt
 echo "//" >> log.txt
 kubectl --namespace=timeserv get pods >> log.txt
-echo "-Log End-..." >> log.txt
+echo "-Log End-" >> log.txt
