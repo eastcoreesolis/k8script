b09f621f735c2e07031caa60151d2dfdd9224ba2
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2020-01-23 08:30:38 -0700

    Create k8s_arm02.sh

---
+++ k8s_arm02.sh
@@ -0,0 +1,23 @@
+echo "-Log Start-..." > log.txt
+echo "apt-get update..." >> log.txt
+sudo apt-get update -y
+echo "//" >> log.txt
+
+echo "Getting Docker..." >> log.txt
+sudo apt-get install docker.io -y
+sudo systemctl enable docker
+sudo systemctl start docker
+
+echo "Adding Repositories..." >> log.txt
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get install kubeadm kubelet kubectl -y --allow-change-held-packages
+sudo apt-mark hold kubeadm kubelet kubectl
+
+echo "Disable Swap..." >> log.txt
+sudo swapoff a
+
+echo "Set Hostname..." >> log.txt
+sudo hostnamectl set-hostname arm02-node
+
+echo "-Log End-" >> log.txt
