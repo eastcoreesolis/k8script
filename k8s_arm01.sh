e5cd9932d11a6115ec04ee5b46dde30ca7dd6a52
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2021-07-25 11:11:19 -0600

    Update k8s_arm01.sh

--- k8s_arm01.sh
+++ k8s_arm01.sh
@@ -8,6 +8,8 @@ sudo apt-get install docker.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
 
+docker volume create jobdata
+
 echo "Adding Repositories..." >> log.txt
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
