66fbd7ba871c8001533a1ab0a13d09f108db841f
eastcoreesolis <55853625+eastcoreesolis@users.noreply.github.com>
2021-07-25 11:16:17 -0600

    Update k8s_head.sh

--- k8s_head.sh
+++ k8s_head.sh
@@ -41,7 +41,10 @@ cd /home/ubuntu/
 mv 'C:\Users\Ginaz\key1.pem' key1.pem
 cd /
 scp -q -o StrictHostKeyChecking=no -i /home/ubuntu/key1.pem ./joincmd.txt ubuntu@$(cat /home/ubuntu/arm01_ip.txt)":"
+scp -q -o StrictHostKeyChecking=no -i /home/ubuntu/key1.pem /home/ubuntu/payload.mp4 ubuntu@$(cat /home/ubuntu/arm01_ip.txt)":"
 scp -q -o StrictHostKeyChecking=no -i /home/ubuntu/key1.pem ./joincmd.txt ubuntu@$(cat /home/ubuntu/arm02_ip.txt)":"
+scp -q -o StrictHostKeyChecking=no -i /home/ubuntu/key1.pem /home/ubuntu/payload.mp4 ubuntu@$(cat /home/ubuntu/arm02_ip.txt)":"
+
 echo "File sent..." >> log.txt
 
 echo "Getting App..." >> log.txt
